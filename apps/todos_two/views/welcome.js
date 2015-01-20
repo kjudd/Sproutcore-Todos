4abb49c75ac4253c5b6467d70ef4902fb124d8be
kjudd <vedette.kristen@gmail.com>
2015-01-19 16:45:33 -0800

    Added additional states.

--- apps/todos_two/views/welcome.js
+++
@@ -1,6 +0,0 @@
-TodosTwo.WelcomeView = SC.LabelView.extend({
-  layout: { centerX: 0, centerY: 0, width: 200, height: 18 },
-  textAlign: SC.ALIGN_CENTER,
-  tagName: "h1", 
-  value: "Welcome to SproutCore!"
-});
